3fbfd4fa9ec1a7c062a16e0bcf79b2f3d83ef9dc
WillMansell <Wombat2000@yahoo.com>
2022-05-09 08:55:56 +0100

    Game state

--- src/main.rs
+++ src/main.rs
@@ -2,12 +2,69 @@
 
 use bracket_lib::prelude::*;
 
-struct State {}
+enum GameMode {
+    Menu,
+    Playing,
+    End,
+}
+
+struct State {
+    mode: GameMode,
+}
+
+impl State {
+    fn new() -> Self {
+        State {
+            mode: GameMode::Menu,
+        }
+    }
+
+    fn play(&mut self, ctx: &mut BTerm){
+        //TODO fill stub
+        self.mode = GameMode::End;
+    }
+
+    fn restart(&mut self){
+        self.mode = GameMode::Playing;
+    }
+
+    fn main_menu(&mut self, ctx: &mut BTerm){
+        ctx.cls();
+        ctx.print_centered( 5, "Welcome to flappy boid");
+        ctx.print_centered( 8, "(P) Play");
+        ctx.print_centered( 9, "(Q) End");
+
+        if let Some(key) = ctx.key {
+            match key{
+            VirtualKeyCode::P => self.restart(),
+            VirtualKeyCode::Q => ctx.quitting = true,
+            _ => {}
+        }
+        }
+    }
+
+    fn dead(&mut self, ctx: &mut BTerm) {
+        ctx.cls();
+        ctx.print_centered(5, "You are dead!");
+        ctx.print_centered(8, "(P) Play Again");
+        ctx.print_centered(9, "(Q) Quit Game");
+        if let Some(key) = ctx.key {
+        match key {
+        VirtualKeyCode::P => self.restart(),
+        VirtualKeyCode::Q => ctx.quitting = true,
+        _ => {}
+        }
+        }
+        }
+}
 
 impl GameState for State {
-    fn tick(&mut self, ctx: &mut BTerm) {
-        ctx.cls();
-        ctx.print(1, 1, "Library check passed");
+    fn tick( &mut self, ctx: &mut BTerm) {
+        match self.mode {
+            GameMode::Menu => self.main_menu(ctx),
+            GameMode::Playing => self.play(ctx),
+            GameMode::End =>self.dead(ctx),
+        }
     }
 }
 
@@ -15,5 +72,5 @@ fn main() -> BError {
     let context = BTermBuilder::simple80x50()
         .with_title("Flappy Dragon")
         .build()?;
-    main_loop(context, State {})
+    main_loop( context, State::new())
 }
